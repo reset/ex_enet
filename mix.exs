2328facf169edc9e68f7e5a971fc2798acdcc96a
Jamie Winsor <jamie@vialstudios.com>
2014-07-02 15:31:29 -0700

    update for Elixir 0.13+

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExEnet.Mixfile do
   def project do
     [ app: :ex_enet,
       version: "0.0.1",
-      elixir: "~> 0.10.1",
+      elixir: "~> 0.13",
       deps: deps,
       compilers: [:rebar, :erlang, :elixir, :app] ]
   end
